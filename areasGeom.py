aec7e46001bf45ca32a0b7e199d4a38f3e97fe26
Ricardo Corsini <corsini.eng@gmail.com>
2024-02-17 20:50:06 -0300

    adição de uma função para área de circunferência

--- areasGeom.py
+++ areasGeom.py
@@ -1,7 +1,13 @@
+from math import pi
+
 def area_ret(ladoA, ladoB):
 
     area = ladoA * ladoB
 
     return area
 
+def area_circ(diametro):
 
+    area = pi * (diametro ** 2) / 4
+
+    return area
